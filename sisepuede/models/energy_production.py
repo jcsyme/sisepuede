Fixed bug in variable cost units conversion

sisepuede/models/energy_production.py
@@ -5720,8 +5720,7 @@ class EnergyProduction:
         regions: Union[List[str], None] = None,
         return_type: str = "table",
     ) -> pd.DataFrame:
-        """
-        Format the CapacityToActivityUnit input table for NemoMod based on 
+        """Format the CapacityToActivityUnit input table for NemoMod based on 
             SISEPUEDE configuration parameters, input variables, integrated 
             model outputs, and reference tables.
 
@@ -5729,8 +5728,9 @@ class EnergyProduction:
         -----------------
         regions : Union[List[str], None]
             Regions to specify. If None, defaults to configuration regions
-        - return_type: "table" or "value". If value, returns only the 
-        CapacityToActivityUnit value for all techs (used in DefaultParams)
+        return_type : str
+            "table" or "value". If value, returns only the 
+            CapacityToActivityUnit value for all techs (used in DefaultParams)
             * Based on configuration parameters
         """
 
@@ -5772,10 +5772,9 @@ class EnergyProduction:
         flag_dummy_price: Union[int, float] = -999,
         minimum_dummy_price: Union[int, float] = 100,
         regions: Union[List[str], None] = None,
-        tables_with_dummy: List[str] = ["CapitalCost", "FixedCost", "VariableCost"]
+        tables_with_dummy: List[str] = ["CapitalCost", "FixedCost", "VariableCost"],
     ) -> pd.DataFrame:
-        """
-        Format the CapitalCost, FixedCost, and VaribleCost input tables for 
+        """Format the CapitalCost, FixedCost, and VaribleCost input tables for 
             NemoMod based on SISEPUEDE configuration parameters, input 
             variables, integrated model outputs, and reference tables.
 
@@ -5786,15 +5785,19 @@ class EnergyProduction:
 
         Keyword Arguments
         -----------------
-        - attribute_fuel: attribute table used for fuels. If None, defaults to 
+        attribute_fuel : Union[AttributeTable, None]
+            AttributeTable used for fuels. If None, defaults to 
             self.model_attributes default
-        - flag_dummy_price: initial price to use, which is later replaced. 
-            Should be a large magnitude negative number.
-        - minimum_dummy_price: minimum price for dummy technologies
+        flag_dummy_price : Union[int, float]
+            Initial price to use, which is later replaced. Should be a large 
+            magnitude negative number.
+        minimum_dummy_price : Union[int, float]
+            Minimum price for dummy technologies
         regions : Union[List[str], None]
             Regions to specify. If None, defaults to configuration regions
-        - tables_with_dummy: list of tables to include dummy tech costs in. 
-            Acceptable values are:
+        tables_with_dummy : List[str]
+            List of tables to include dummy tech costs in. Acceptable values 
+            are:
 
             * "CapitalCost"
             * "FixedCost"
@@ -5809,15 +5812,15 @@ class EnergyProduction:
         )
         pycat_enfu = self.model_attributes.get_subsector_attribute(
             self.model_attributes.subsec_name_enfu,
-            "pycategory_primary_element"
+            "pycategory_primary_element",
         )
 
         dict_return = {}
         flag_dummy_price = min(flag_dummy_price, -1)
 
         # get some scalars (monetary and power)
-        scalar_cost_capital = self.model_attributes.get_scalar(self.modvar_entc_nemomod_capital_cost, "monetary")
-        scalar_cost_capital /= self.model_attributes.get_scalar(self.modvar_entc_nemomod_capital_cost, "power")
+        scalar_cost_capital = self.model_attributes.get_scalar(self.modvar_entc_nemomod_capital_cost, "monetary", )
+        scalar_cost_capital /= self.model_attributes.get_scalar(self.modvar_entc_nemomod_capital_cost, "power", )
         scalar_cost_fixed = self.model_attributes.get_scalar(self.modvar_entc_nemomod_fixed_cost, "monetary")
         scalar_cost_fixed /= self.model_attributes.get_scalar(self.modvar_entc_nemomod_fixed_cost, "power")
         scalar_cost_variable = self.model_attributes.get_scalar(self.modvar_entc_nemomod_variable_cost, "monetary")
@@ -5885,14 +5888,15 @@ class EnergyProduction:
         ##  VariableCost -- Pull Variable O&M and Add Fuel Costs
 
         # get fuel costs -- specify energy & monetary units in terms of self.modvar_entc_nemomod_variable_cost
-        units_enfu_costs_monetary = self.model_attributes.get_variable_characteristic(
-            self.modvar_entc_nemomod_variable_cost,
-            self.model_attributes.varchar_str_unit_monetary
-        )
+        # units_enfu_costs_monetary = self.model_attributes.get_variable_characteristic(
+        #     self.modvar_entc_nemomod_variable_cost,
+        #     self.model_attributes.varchar_str_unit_monetary
+        # )
         arr_enfu_costs = self.model_enercons.get_enfu_fuel_costs_per_energy(
             df_enerprod_trajectories,
-            modvar_for_units_energy = self.modvar_entc_nemomod_variable_cost,
-            units_monetary = units_enfu_costs_monetary
+            #modvar_for_units_energy = self.modvar_entc_nemomod_variable_cost,
+            units_energy = self.model_attributes.configuration.get("energy_units_nemomod"),
+            units_monetary = self.model_attributes.configuration.get("monetary_units"),
         )
 
         # get variable costs, add fuel costs, and create data frame to pass
@@ -5925,7 +5929,10 @@ class EnergyProduction:
             ind_enfu = attr_enfu.get_key_value_index(cat_enfu)
             field_varcost = list(df_entc_variable_costs.columns)[j]
             
-            df_entc_variable_costs[field_varcost] = np.array(df_entc_variable_costs[field_varcost]) + arr_enfu_costs[:, ind_enfu]
+            df_entc_variable_costs[field_varcost] = (
+                scalar_cost_variable*np.array(df_entc_variable_costs[field_varcost]) 
+                + arr_enfu_costs[:, ind_enfu]
+            )
         
         #
         # dummy techs are high-cost technologies that help ensure there is no unmet demand in the system if other constraints create an issue
@@ -5956,7 +5963,7 @@ class EnergyProduction:
                 df_append = df_append,
                 dict_fields_to_pass = {self.field_nemomod_mode: self.cat_enmo_gnrt},
                 regions = regions,
-                scalar_to_nemomod_units = scalar_cost_variable,
+                # scalar_to_nemomod_units = scalar_cost_variable,
                 var_bounds = (0, np.inf)
             )
         )